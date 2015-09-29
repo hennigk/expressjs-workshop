9d2253ce3dab6389ebb31e55b7f3a727f28cc2f1
hennigk <kayla.hennig@gmail.com>
2015-09-29 18:57:57 +0000

    exercise-4

--- index.js
+++ index.js
@@ -1,11 +1,60 @@
 var express = require('express');
 var app = express();
 
+var entries = {
+  1: {
+    firstName: "Kayla",
+    lastName: "Hennig",
+    emails: [
+      {type: "home", address: "kayla@home.com"},
+      {type: "work", address: "kayla@work.com"},
+    ]
+  },
+  2: {
+    firstName: "David",
+    lastName: "Fortin",
+    emails: [
+      {type: "home", address: "dave@home.com"},
+      {type: "work", address: "dave@work.com"},
+    ]
+  },
+  3: {
+    firstName: "Beijo",
+    lastName: "The Dog",
+    address: [
+      {type: "home", address: "2 crate lane"},
+      {type: "work", address: "dog park lane"}]
+  },
+  4: {
+    firstName: "Boots",
+    lastName: "The Cat",
+    address: [
+      {type: "home", address: "the blanket in the closet"},
+      {type: "work", address: "the blanket by the window"}]
+  }
+}
+
+var count = 0;
+for (var key in entries) {
+  count +=1;
+}
+  
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send("welcome to my server!");
 });
 
-
+app.get('/entry/:entryId', function (req, res) {
+  var entry = Number(req.params.entryId)
+  if (isNaN(entry)) {
+    res.status(404).send("Error 404: invalid entry number");
+  }
+  else if ((entry > count) || (entry < 1) ){
+    res.status(404).send("Error: Enter a number between 1 and " + count);
+  }
+  else {
+    res.json(entries[entry]);
+  }
+});
 
 
 
